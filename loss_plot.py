66f360557f8281a61407d53ffdb211fdaaa223f4
Yuan <804547696@qq.com>
2020-06-17 09:55:57 +0800

    fix plot loss function

--- loss_plot.py
+++ loss_plot.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def loss_plot(N, K, path = ".\\mylog", keyword = "train")
+def loss_plot(N, K, path = ".\\mylog", keyword = "train"):
     """
     
     Args:
@@ -23,13 +23,15 @@ def loss_plot(N, K, path = ".\\mylog", keyword = "train")
         raise ValueError("keyword must be 'train' or 'test' or 'acc'")
 
     step = np.load(path + "{}-way-{}-shot_step.npz".format(N, K))
-    fig, ax = plt.figure()
+    fig, ax = plt.subplots()
     if keyword in ["train", "test"]:
         ax.plot(step, loss, label = "{} loss".format(keyword))
         ax.set_xlabel('step')
         ax.set_xlabel("{} loss".format(keyword))
+        fig.save("{}_loss_of_{}-way-{}-shot".format(keyword, N, K))
     else:
         ax.plot(step, acc, label = "Accuracy")
         ax.set_xlabel('step')
         ax.set_xlabel("{}".format(keyword))
+        fig.save("{}_of_{}-way-{}-shot".format(keyword, N, K))
 
